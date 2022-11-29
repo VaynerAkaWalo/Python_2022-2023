878df7eacb57d39675739002a6f3a4ca53ad8b59
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-11-29 23:25:03 +0100

    Revert "zestaw7+README"
    This reverts commit b692f666b2b5589503674ca2ae07c5956a749340.

--- Zestaw7/iterators.py
+++
@@ -1,6 +0,0 @@
-import itertools
-import random
-
-iterator01 = itertools.cycle(range(0, 2))
-iteratorBladzenieprzypadkowe = iter(iter((lambda: random.choice(("N", "E", "S", "W"))), 0))
-iteratordni = itertools.cycle(range(0, 7))
